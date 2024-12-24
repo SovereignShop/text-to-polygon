17620d97b823e4b283e40aba93dfcba3e43beae3
John Collins <jmicahc@gmail.com>
2024-12-23 23:20:34 -0800

    fix

--- include/text_to_polygon.h
+++ include/text_to_polygon.h
@@ -6,22 +6,24 @@
 #include <string>
 #include <ft2build.h>
 #include FT_FREETYPE_H
-#include <glm/vec2.hpp>
 
 
 using namespace std;
+using vec2 = linalg::vec<double, 2>;
+using vec3 = linalg::vec<double, 3>;
+using vec4 = linalg::vec<double, 4>;
 
 namespace TextToPolygon {
 
-glm::vec2 interpolateQuadraticBezier(const glm::vec2& P0, const glm::vec2& P1, const glm::vec2& P2, float t);
+vec2 interpolateQuadraticBezier(const vec2& P0, const vec2& P1, const vec2& P2, float t);
 
-std::vector<glm::vec2> interpolateQuadraticBezierSegment(const glm::vec2& start, const glm::vec2& control, const glm::vec2& end, int resolution);
+std::vector<vec2> interpolateQuadraticBezierSegment(const vec2& start, const vec2& control, const vec2& end, int resolution);
 
-std::vector<glm::vec2> processContour(const std::vector<FT_Vector>& outlinePoints, const std::vector<char>& outlineTags, int contourStartIdx, int contourEndIdx, int resolution, float xOffset);
+std::vector<vec2> processContour(const std::vector<FT_Vector>& outlinePoints, const std::vector<char>& outlineTags, int contourStartIdx, int contourEndIdx, int resolution, float xOffset);
 
-std::vector<std::vector<glm::vec2>> processOutline(const FT_Outline& outline, int resolution, float xOffset);
+std::vector<std::vector<vec2>> processOutline(const FT_Outline& outline, int resolution, float xOffset);
 
-std::vector<std::vector<glm::vec2>> textToPolygons(const std::string& fontFile, const std::string& text, u_int32_t pixelHeight, int interpRes);
+std::vector<std::vector<vec2>> textToPolygons(const std::string& fontFile, const std::string& text, u_int32_t pixelHeight, int interpRes);
 
 } // namespace TextToPolygon
 
